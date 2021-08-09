dc6d1e2d922ccdbcfb21d2a8c3fef3f5d47d4337
Raj kumar Maurya <rajkumar25maurya@gmail.com>
2021-08-09 14:35:55 +0530

    lamda function is created

--- lambda.tf
+++ lambda.tf
@@ -49,7 +49,7 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 resource "aws_lambda_function" "lambda_tf" {
     filename    =   "lambda.zio"
     function_name = "lambda_handler"
-    role = $"{aws_iam_role.iam_for_lambda.arn}"
+    role = "${aws_iam_role.iam_for_lambda.arn}"
     handler         = "lambda.lambda_handler"
     runtime         = "python3.7"
     source_code_hash = "${filebase64sha256("lambda.zip")}"
